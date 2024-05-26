ac0303320df7138f702507d8c9df8b9d333692f5
johandry8919 <rojas.rojas440@gmail.com>
2024-05-25 21:53:12 -0400

    asset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,8 +24,6 @@
 </head>
 
 <body>
-
-
     <!-- Page Preloder -->
     <div id="preloder">
         <div class="loader"></div>
@@ -127,14 +125,14 @@
         </div>
     </div>
 
-    <script src="js/jquery-3.3.1.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/player.js"></script>
-    <script src="js/jquery.nice-select.min.js"></script>
-    <script src="js/mixitup.min.js"></script>
-    <script src="js/jquery.slicknav.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/main.js"></script>
+    <script src={{asset('js/jquery-3.3.1.min.js')}}></script>
+    <script src={{asset('js/bootstrap.min.js')}}></script>
+    <script src={{asset('js/player.js')}}></script>
+    <script src={{asset('js/jquery.nice-select.min.js')}}></script>
+    <script src={{asset('js/mixitup.min.js')}}></script>
+    <script src={{asset('js/jquery.slicknav.js')}}></script>
+    <script src={{asset('js/owl.carousel.min.js')}}></script>
+    <script src={{asset('js/main.js')}}></script>
 </body>
 
 </html>
